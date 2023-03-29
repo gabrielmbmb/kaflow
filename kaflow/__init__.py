47045188bcbf22de6d15cf38aba1a0e48d849f1d
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-03-29 23:26:14 +0200

    :sparkles: Add `ProtobufSerializer` (#5)
    * :heavy_plus_sign: Add `protobuf>=3.20.0`
    
    * :sparkles: Add `ProtobufSerializer`

--- kaflow/__init__.py
+++ kaflow/__init__.py
@@ -1,6 +1,16 @@
 from kaflow.applications import Kaflow
-from kaflow.serializers import Avro, Json
+from kaflow.serializers import Json, has_fastavro, has_protobuf
 
-__all__ = ["Kaflow", "Json", "Avro"]
+__all__ = ["Kaflow", "Json"]
+
+if has_fastavro:
+    from kaflow.serializers import Avro  # noqa: F401
+
+    __all__.append("Avro")
+
+if has_protobuf:
+    from kaflow.serializers import Protobuf  # noqa: F401
+
+    __all__.append("Protobuf")
 
 __version__ = "0.0.1"
